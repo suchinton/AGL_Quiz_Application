d21febe3b8f95ed9398e172d27d45a6a56b8960d
Suchinton <75079303+suchinton@users.noreply.github.com>
2024-03-24 22:15:44 +0530

    Update App theme and default Kuksa Config

--- suchintonApp.py
+++ suchintonApp.py
@@ -1,52 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QSlider, QLCDNumber, QPushButton, QLabel, QTextBrowser
+from PyQt5.QtWidgets import QApplication, QSlider, QLCDNumber, QPushButton, QLabel
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QBitmap, QPainter, QColor
 from PyQt5 import uic
 
 import kuksa_client as kc
 
 import os
-from AppRes_rc import *
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 Form, Base = uic.loadUiType(os.path.join(current_dir, "Main.ui"))
 
 import sys
-import platform
 
-python_version = f"python{'.'.join(platform.python_version_tuple()[:2])}"
-
-def check_paths(*paths):
-    ans = {path: os.path.exists(path) for path in paths}
-    # reverse the keys and values
-    return {v: k for k, v in ans.items()}
-
-CA_PATHS = check_paths(
-    "/etc/kuksa-val/CA.pem",
-    f"/usr/lib/{python_version}/site-packages/self.client/kuksa_server_certificates/CA.pem",
-    os.path.abspath(os.path.join(os.path.dirname(
-        __file__), "./cert/CA.pem"))
-)
-
-GRPC_TOKEN_PATHS = check_paths( 
-    os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                 "./token/grpc/actuate-provide-all.token"))
-)
-
-class Worker(QThread):
-    error_occurred = pyqtSignal(str)
-
-    def __init__(self, client, speed):
-        super().__init__()
-        self.client = client
-        self.speed = speed
-
-    def run(self):
-        try:
-            self.client.setValue('Vehicle.Speed', str(self.speed), 'value')
-        except Exception as e:
-            self.error_occurred.emit(str(e))
 class MainWindow(Base, Form):
     def __init__(self):
         super().__init__()
@@ -57,10 +23,7 @@ class MainWindow(Base, Form):
         "ip": 'localhost',
         "port": 55555,
         "protocol": "grpc",
-        "insecure": False,
-        "tls_server_name": "Server",
-        "cacert": CA_PATHS.get(True),
-        "token": GRPC_TOKEN_PATHS.get(True),
+        "insecure": True,
         }
 
         self.SpeedSlider = self.findChild(QSlider, 'SpeedSlider')
@@ -68,40 +31,46 @@ class MainWindow(Base, Form):
         
         self.ProfilePic = self.findChild(QLabel, 'ProfilePic')
         self.SwitchPP = self.findChild(QPushButton, 'SwitchPP')
-        self.OutputBox = self.findChild(QTextBrowser, 'OutputBox')
 
         self.SpeedSlider.valueChanged.connect(self.changeValue)
         self.SwitchPP.clicked.connect(self.changePP)
 
+        self.ProfilePic.setPixmap(self.roundEdges(self.ProfilePic.pixmap()))
 
         try:
             self.client = kc.KuksaClientThread(config)
             self.client.start()
         except Exception as e:
             print(f"Error: {e}")
-            self.OutputBox.append("------------Error!!----------")
 
     def changeValue(self, value):
-        speed = int(self.SpeedSlider.value())
-        self.worker = Worker(self.client, speed)
-        self.worker.error_occurred.connect(self.handle_error)
-        self.worker.start()
-
-    def handle_error(self, error_message):
-        print("Error!! self.client not configured properly.")
-        self.OutputBox.append("------------Error!!----------")
-        self.OutputBox.append(error_message)
-        self.lcdNumber.display(int(self.client.getValue('Vehicle.Speed','value')))
-
+        speed = int(value)
+        try:
+            self.client.setValue('Vehicle.Speed',str(speed),'value')
+            print(f"Feeding Vehicle.Speed to {speed}")
+            self.lcdNumber.display(int(self.client.getValue('Vehicle.Speed','value')))
+        except:
+            print("Error!! kuksa_client not configured properly.")
 
     def changePP(self):
-        PP1 = QPixmap(":/images/images/BG1.jpg")
-        PP2 = QPixmap(":/images/images/BG2.png")
+        PP1 = self.roundEdges(QPixmap(":/images/images/BG1.jpg"))
+        PP2 = self.roundEdges(QPixmap(":/images/images/BG2.png"))
 
         if self.ProfilePic.pixmap().toImage() == PP1.toImage():
             self.ProfilePic.setPixmap(PP2)
         else:
             self.ProfilePic.setPixmap(PP1)
+
+    def roundEdges(self, pixmap):
+        mask = QBitmap(pixmap.size())
+        mask.fill()
+        painter = QPainter(mask)
+        painter.setBrush(QColor(0, 0, 0))
+        painter.setPen(QColor(0, 0, 0))
+        painter.drawRoundedRect(0, 0, pixmap.width(), pixmap.height(), 100, 100)
+        painter.end()
+        pixmap.setMask(mask)
+        return pixmap
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
